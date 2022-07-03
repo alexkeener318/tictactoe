910e1e02e1020469f51d3c28e7b850e721225dee
Alex Keener <alexkeener318@gmail.com>
2022-07-02 23:24:43 -0500

    Adds player object

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const player = () => {
+    let moves = [];
+    const symbol = "";
+
+    const setSymbol = symbol => {
+        this.symbol = symbol;
+    }
+
+    const addMove = () => {
+
+    }
+}
